fb8f662f3c0dacaf13013adb9686260157e6c272
earbeitman <evanarbeitman@gmail.com>
2019-01-13 16:27:29 -0500

    Create model for services app and ran migrations

--- services/models.py
+++ services/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Meme(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField()
+    first_visited_date = models.DateField()
+    rank = models.IntegerField()
