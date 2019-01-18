1d8a4942061aa5f173d155af1587013e6ccf96b9
earbeitman <evanarbeitman@gmail.com>
2019-01-17 22:26:23 -0500

    app loads but root displays nothing

--- PikaPapow/settings.py
+++ PikaPapow/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'services',
     'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/services', views.MemeListCreate.as_view()),
+    path('', views.index),
 ]
