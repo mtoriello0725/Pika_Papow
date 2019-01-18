70ff93458b8f45c4aee5403cddb216ddf5939b79
earbeitman <evanarbeitman@gmail.com>
2019-01-17 20:29:47 -0500

    Add services url to project add url path to services

--- PikaPapow/settings.py
+++ PikaPapow/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'services',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'services',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
