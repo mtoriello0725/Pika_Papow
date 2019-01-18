a9ab51f70cb56837c93f473cf03968703c7ebb8a
earbeitman <evanarbeitman@gmail.com>
2019-01-17 20:30:53 -0500

    add genericAPIView to services view

--- services/views.py
+++ services/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from services.models import Meme
+from services.serializers import MemeSerializer
+from rest_framework import generics
 # Create your views here.
+
+class MemeListCreate(generics.ListCreateAPIView):
+    queryset = Meme.objects.all()
+    serializer_class = MemeSerializer
